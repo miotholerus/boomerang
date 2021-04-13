dcae1a9a5c4706bc61148046d91fb5d9dfed9c14
miotholerus <mio.tholerus@stud.sti.se>
2021-04-13 22:32:07 +0200

    mer stilfix i create-schedule //mio

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div className="header">
       <i className="fa fa-bars"></i>
-      <span className="banner">Boomerang</span>
-      <span className="my-account right">Siri</span>
+      <span className="banner"> Boomerang </span>
+      <span className="my-account right">Siri </span>
       <i className="fa fa-user-circle right menu-user-icon"></i>
     </div>
   )
