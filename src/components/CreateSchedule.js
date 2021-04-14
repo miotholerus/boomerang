51d5e49adda86d101444f9f01a3baaca88d4cbe1
JavaByMohamed <mohamed_abdel.monem@hotmail.se>
2021-04-14 09:36:58 +0200

    Mo14/04

--- src/components/CreateSchedule.js
+++ src/components/CreateSchedule.js
@@ -3,10 +3,57 @@
 
 export default function CreateSchedule() {
   return (
-    <div>
-      {/* html för formulär
+
+
+    <div >
+
+      <div className="container create-schedule-box">
+        <form>
+          <b>Skapa körschema</b>
+          <label for="container create-schedule-box"><br></br>Fyll i destination: </label>
+          <input type="text" class="s" name="new" />
+          <br></br><br></br>
+
+        </form>
+      </div>
+
+  
+      {/* <button onclick="CreateSchedule()" class="dropbtn">Välj dag för skjustning</button> */}
       
-      */}
+        
+      
+      {/* <div className="s">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <label for="container create-schedule-box">Måndag</label>
+        <input type="radio" className="s" name="new" /><br></br>
+        <label for="container create-schedule-box">Tisdag</label>
+        <input type="radio" className="s" name="new" /><br></br>
+        <label for="container create-schedule-box">Onsdag</label>
+        <input type="radio" className="s" name="new" /><br></br>
+        <label for="container create-schedule-box">Torsdag</label>
+        <input type="radio" className="s" name="new" /><br></br>
+        <label for="container create-schedule-box">Fredag</label>
+        <input type="radio" className="s" name="new" /><br></br>
+        <label for="container create-schedule-box">Lördag</label>
+        <input type="radio" className="s" name="new" /><br></br>
+        <label for="container create-schedule-box">Söndag</label>
+        <input type="radio" className="s" name="new" /><br></br>
+  
+      </div> */}
+ 
     </div>
   )
 }
+
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
