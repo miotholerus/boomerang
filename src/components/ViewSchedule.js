1151fae5a7f29e336b0f71ab36d3b27ab88f4dfd
miotholerus <mio.tholerus@stud.sti.se>
2021-04-15 12:22:43 +0200

    15 april mio

--- src/components/ViewSchedule.js
+++ src/components/ViewSchedule.js
@@ -1,9 +1,11 @@
 ﻿import React from 'react'
+import Header from './Header'
 import RideOccation from './RideOccation'
 
 export default function ViewSchedule({schedule}) {
     return (
-        null
+        <Header/>
+        
         // egentligen bör vi hämta och mappa en lista av occations ur schedule, snarare än direkt över schedule
         // schedule.map(occation => {
         //     return <RideOccation key={occation.id} occation={occation} />
