145431871c8d62e3cbac6d27d9ce76443769c542
miotholerus <mio.tholerus@stud.sti.se>
2021-04-27 18:38:27 +0200

    Kan skriva in adress och få ett response :> //Mio

--- src/components/TestAPI.js
+++ src/components/TestAPI.js
@@ -2,8 +2,8 @@ import React, {useState, usRef} from 'react'
 
 export default function TestAPI() {
 
-    const [origin, setOrigin] = useState("59.26078347858798,18.02139952888256"); // "Lerbäcksgränd 18, 124 66 Bandhagen"
-    const [destination, setDestination] = useState("59.28772349484027,18.058070840530874"); // "Sockenvägen 290, 122 63 Enskede"
+    const [origin, setOrigin] = useState(""); // "59.26078347858798,18.02139952888256" "Lerbäcksgränd 18, 124 66 Bandhagen"
+    const [destination, setDestination] = useState(""); // "59.28772349484027,18.058070840530874" "Sockenvägen 290, 122 63 Enskede"
 
     const url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=bp6pjpgmaCcdxjtccQlnDpjTPEUqk`;
 
@@ -21,14 +21,14 @@ export default function TestAPI() {
 
     return (
         <form onSubmit={(event) => submitForm(event)}>
-            {/* <div>
-                <label for="title">Title: </label>
-                <input type="text" id="title" value={origin} onChange={(event) => setOrigin(event.target.value)}/>
+            <div>
+                <label for="origin">Från: </label>
+                <input type="text" id="origin" value={origin} onChange={(event) => setOrigin(event.target.value)}/>
             </div>
             <div>
-                <label for="content">Content: </label>
-                <textarea type="text" id="content" value={destination} onChange={(event) => setDestination(event.target.value)}/>
-            </div> */}
+                <label for="destination">Till: </label>
+                <input type="text" id="destination" value={destination} onChange={(event) => setDestination(event.target.value)}/>
+            </div>
             <div>
                 <input type="submit" id="submitbutton" value="Send" />
             </div>
