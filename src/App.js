b59043b770a60409deda3e54a7791b78ed2372a6
miotholerus <mio.tholerus@stud.sti.se>
2021-04-28 11:03:55 +0200

    28 april //Mio

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
     }
   ]);
   
-  // Schemat med grundvärden inlaggt (för att det inte ska krascha när vi testar saker och man inte angett
+  // Schemat med grundvärden inlaggt (för att det inte ska krascha när vi testar saker och man inte har fyllt i formuläret)
   //                                   index:  0        1            2          3         4             5       6
   const [schedule, setSchedule] = useState(["00:00", "00:00", "Sergelgatan 1", "0", "0000-00-00", "0000-00-00", [] ]);
   // 0 - starttid
--- src/components/DetailedSchedule.js
+++ src/components/DetailedSchedule.js
@@ -3,7 +3,10 @@ import ScheduleBox from './ScheduleBox'
 
 export default function DetailedSchedule({schedule, members}) {
   const destinationFromSchedule = schedule[2];
-  const addressFromMember = members[0].address; // schedule[]
+  const addressFromMember = members[0].address; // ska senare hämtas direkt från schedule-rideobjecten
+  // schedule[6] = lista av rideobjects
+  // schedule[6][0] = första rideobjectet i listan
+  // schedule[6][0].driverTo.address = rideobjectets förares adress
   const exampleData = {
     "destination_addresses": [
       "Sockenvägen 290, 122 63 Enskede, Sweden"
@@ -29,12 +32,12 @@ export default function DetailedSchedule({schedule, members}) {
       }
     ],
     "status": "OK"
-  }
+  } // exempeldata för att det inte ska krascha när vi testar
 
-  const [origin, setOrigin] = useState(addressFromMember);
-  const [destination, setDestination] = useState(destinationFromSchedule);
-  const [data, setData] = useState(exampleData);
-  const [seconds, setSeconds] = useState(0);
+  const [origin, setOrigin] = useState(addressFromMember); // kommer ej behövas sen, det är samma sak som addressFromMember
+  const [destination, setDestination] = useState(destinationFromSchedule); // kommer ej behövas sen, det är samma sak som destinationFromSchedule
+  const [data, setData] = useState(exampleData); // den riktiga datan efter att man kört API:n (kommer behöva köras för varje delresa)
+  const [seconds, setSeconds] = useState(0); // sekunderna från datan, ska dras av från klockslagen
 
   const url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=bp6pjpgmaCcdxjtccQlnDpjTPEUqk`;
 
@@ -42,7 +45,7 @@ export default function DetailedSchedule({schedule, members}) {
     console.log(origin);
     console.log(destination);
     console.log(data.rows[0].elements[0].duration.value)
-    setSeconds(data.rows[0].elements[0].duration.value)
+    setSeconds(data.rows[0].elements[0].duration.value) // seconds laddas från json-data
   }, [data])
 
   const submitForm = (event) => {
