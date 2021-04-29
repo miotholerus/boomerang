fc947469bb125f9c57d69f58ffaa654da10a46fe
miotholerus <mio.tholerus@stud.sti.se>
2021-04-29 17:33:36 +0200

    Tidsfunktion i TestAPI 29 april

--- src/components/TestAPI.js
+++ src/components/TestAPI.js
@@ -3,18 +3,34 @@ import React, { useState, usRef, useEffect } from 'react'
 
 export default function TestAPI() {
   
-  const []
+  const [timeAtoB, setTimeAtoB] = useState(0);
+  const [timeBtoC, setTimeBtoC] = useState(0);
+  const [timeCtoD, setTimeCtoD] = useState(0);
+
+  var addSeconds = function (dateTime, seconds) {
+    return new Date(dateTime.getTime() + seconds * 1000);
+  }
+
+  var startTime = new Date(new Date("2021-04-29").setHours(17, 0));
+
+  var margin = 5*60;
   
+  var timeAtA = addSeconds(startTime, -(timeAtoB + margin + timeBtoC + margin + timeCtoD + margin)).toLocaleTimeString('sv-se')//.substring(0, 5);
+  var timeAtB = addSeconds(startTime, -(timeBtoC + margin + timeCtoD + margin)).toLocaleTimeString('sv-se');
+  var timeAtC = addSeconds(startTime, -(timeCtoD + margin)).toLocaleTimeString('sv-se');
+  var timeAtD = addSeconds(startTime, -margin).toLocaleTimeString('sv-se');//.substring(0, 5);
+
   const url = `https://www.mapquestapi.com/directions/v2/routematrix?key=Kr8qYQFLlQ0XjbF6hGWyyTprB5x4fEPU`;
   
 
   const submitForm = async (event) => {
     event.preventDefault();
 
-    const locA = "59.28093793567633, 18.046790156144745"; //  Uddeholmsvägen 239, 12241 Enskede, SWEDEN
-    const locB = "59.272215063170705, 18.037266160912903"; // Årdalavägen 133, 12432 Bandhagen, SWEDEN
-    const locC = "59.26093702587597, 18.021367342650105"; // Lerbäcksgränd 18, 12466 Bandhagen, Sweden
-    const locD = "59.287761851370426, 18.05808156963875"; // Sockenvägen 290, 12040 Enskede, SWEDEN
+    const locA = "59.28093793567633, 18.046790156144745"; //  "Uddeholmsvägen 239, 12241 Enskede, SWEDEN";
+    const locB = "59.272215063170705, 18.037266160912903"; // "Årdalavägen 133, 12432 Bandhagen, SWEDEN";
+    const locC = "59.26093702587597, 18.021367342650105"; // "Lerbäcksgränd 18, 12466 Bandhagen, SWEDEN";
+    const locD = "59.287761851370426, 18.05808156963875"; // "Sockenvägen 290, 12040 Enskede, SWEDEN";
+
 
     const requestBodyAtoB = {
       "locations": [
@@ -31,7 +47,10 @@ export default function TestAPI() {
     })
     const dataAtoB = await responseAtoB.json()
 
-    console.log(dataAtoB)
+    console.log(dataAtoB);
+
+    setTimeAtoB(dataAtoB.time[1]);
+
 
 
     const requestBodyBtoC = {
@@ -51,6 +70,9 @@ export default function TestAPI() {
 
     console.log(dataBtoC)
     
+    setTimeBtoC(dataBtoC.time[1]);
+
+
     
     const requestBodyCtoD = {
       "locations": [
@@ -69,20 +91,28 @@ export default function TestAPI() {
 
     console.log(dataCtoD)
 
+    setTimeCtoD(dataCtoD.time[1]);
+
+    
   }
 
   return (
     <form onSubmit={(event) => submitForm(event)}>
-      {/* <div>
-        <label for="origin">Från: </label>
-        <input type="text" id="origin" value={origin} onChange={(event) => setOrigin(event.target.value)} />
-      </div>
-      <div>
-        <label for="destination">Till: </label>
-        <input type="text" id="destination" value={destination} onChange={(event) => setDestination(event.target.value)} />
-      </div> */}
       <div>
         <input type="submit" id="submitbutton" value="Test API" />
+      </div>
+      <div>
+        Starttid aktivitet: {startTime.toLocaleTimeString("sv-se")}<br></br>
+        Marginal: {margin/60} min<br></br><br></br>
+
+        A till B: {timeAtoB} sek ({timeAtoB/60} min)<br></br>
+        B till C: {timeBtoC} sek ({timeBtoC/60} min)<br></br>
+        C till D: {timeCtoD} sek ({timeCtoD/60} min)<br></br><br></br>
+        
+        A kl. {timeAtA}<br></br>
+        B kl. {timeAtB}<br></br>
+        C kl. {timeAtC}<br></br>
+        D kl. {timeAtD}
       </div>
     </form>
   )
