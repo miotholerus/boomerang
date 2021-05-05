0617b5182cd84e00ca7f04c5f35fbd7dcfd1c3c7
miotholerus <mio.tholerus@stud.sti.se>
2021-05-05 15:04:00 +0200

    Nytt försök CRUD

--- src/components/TestDatabase.js
+++ src/components/TestDatabase.js
@@ -10,34 +10,37 @@ export default function TestDatabase() {
   const [address, setAddress] = useState("");
 
   
-  function createMember() {
+  function createMember(e) {
+    e.preventDefault();
+    console.log("Kör createMember");
+
     const membersRef = firebase.database().ref("Members");
   
     const member = {
       firstName,
       lastName,
       child,
-      address,
+      address
     }
   
     membersRef.push(member); // lägger till i listan över members
   }  
 
   return (
-    <form>
-      <label for="first-name">Förnamn</label><br></br>
+    <form onSubmit={(e) => createMember(e)}>
+      <label htmlFor="first-name">Förnamn</label><br></br>
       <input type="text" id="first-name" onChange={e => setFirstName(e.target.value)} value={firstName}/><br></br>
       
-      <label for="last-name">Efternamn</label><br></br>
+      <label htmlFor="last-name">Efternamn</label><br></br>
       <input type="text" id="last-name" onChange={e => setLastName(e.target.value)} value={lastName}/><br></br>
       
-      <label for="child">Barnets namn</label><br></br>
+      <label htmlFor="child">Barnets namn</label><br></br>
       <input type="text" id="child" onChange={e => setChild(e.target.value)} value={child}/><br></br>
 
-      <label for="address">Adress</label><br></br>
+      <label htmlFor="address">Adress</label><br></br>
       <input type="text" id="address" onChange={e => setAddress(e.target.value)} value={address}/><br></br>
 
-      <button onClick={createMember}>Registrera</button>
+      <input type="submit" id="hejhopp" value="Registrera"/>
     </form>
   )
 }
