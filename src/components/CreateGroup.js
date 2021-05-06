676618ad9d9a01c9a98b2b9f323f1aa99c742993
JavaByMohamed <mohamed_abdel.monem@hotmail.se>
2021-05-06 15:21:13 +0200

    MO

--- src/components/CreateGroup.js
+++ src/components/CreateGroup.js
@@ -22,41 +22,6 @@ export default function CreateGroup() {
       <Header />
       <div className="page-content">
         <div>
-<<<<<<< HEAD
-            <Header />
-            <div className="page-content">
-                <div>
-                    <div className="sub-header-football">
-                        <div className="leftcorner">
-                            <AiOutlineLeft />
-                        </div>
-                        <div className="middel">
-                            <h4>Skapa grupp</h4>
-                        </div>
-                        <div className="rightcorner">
-                            <AiOutlineClose />
-                        </div>
-                    </div>
-                    <div className="blackbackground">
-                        <div className="whitebackground">
-                            <form><br></br>
-                                <p className="text">Namn på grupp:</p>
-                                <input type="text" className="creategroupinfo borderradius" placeholder="Ex. Badmintongruppen"></input>
-                                <p className="text">Meddelandetext:</p>
-                                <textarea className="messagetext borderradius" value="Exempeltext:
-Hej, här är en inbjudan till gruppen (namn) som vi pratade om. 
-Gå med i gruppen så skapar vi ett gemensant körschema. Du registrar dig på boomerang.nu
-Mvh (namn)"></textarea><br></br>
-                                <p className="text">Bjud in med mail eller mobilnr</p>
-                                <input type="text" className="creategroupinfo borderradius" placeholder="Fyll i mail eller mobilnummer"></input><br></br>
-                                <button type="button" className='button-v2 '>
-                                    <b>LÄGG TILL PERSONER</b>
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-=======
 
           <div className="sub-header-football">
             <div className="leftcorner">
@@ -92,7 +57,6 @@ Mvh (namn)"></textarea><br></br>
                 </button>
 
               </form>
->>>>>>> mio
 
             </div>
           </div>
