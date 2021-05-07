f10f5875aaeb3da91d3cc4eed615563f25a69dc7
LukasEjnarsson <73545187+LukasEjnarsson@users.noreply.github.com>
2021-05-07 10:41:36 +0200

    7/5

--- src/components/MinaSidor.js
+++ src/components/MinaSidor.js
@@ -28,9 +28,18 @@ export default function MinaSidor({ me, groups }) {
           <div className="infogrupp">
             <p>Du är inte med i någon grupp än. Skapa en ny grupp och ett nytt körschema.</p>
 
-            <Link className="button-grupp" to='/creategroup'>SKAPA NY GRUPP</Link>
+            <Link className="button-v2" to='/creategroup'>SKAPA NY GRUPP</Link>
           </div>
         </>}
+        <div>
+                    <h3 className="topline_2">Så kommer du igång</h3>
+                    <ol className="how_to" start="1">
+                        <li className="info_text">Skapa en ny grupp</li>
+                        <li className="info_text">Lägg till medlemar</li>
+                        <li className="info_text">Skapa ett körschema</li>
+                    </ol>
+                    
+                </div>
 
 
     </div>
